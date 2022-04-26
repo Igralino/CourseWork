63c134a9b17f448f4d58a049b472e017a76e5729
Igor Iunash <Igralino@ya.ru>
2022-04-26 13:49:47 +0300

    create ios app

---
+++ TQA_App/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Igor Iunash on 26.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TQA_App/Shared/TQA_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TQA_AppApp.swift
+//  Shared
+//
+//  Created by Igor Iunash on 26.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TQA_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
